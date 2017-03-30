5f742bbf5f80fb4d6b7c79c158b1bedccbe03ed7
akshayhonap619 <honap.a@husky.neu.edu>
2017-03-30 03:17:01 -0400

    Most of addCourse done

--- OCMS/src/UniversityPage.java
+++ OCMS/src/UniversityPage.java
@@ -1,7 +1,10 @@
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class UniversityPage 
 {
+	public enum designation {Dean,Professor,Associate_Professor,Lecturer,Visiting_Scholar,Director,Associate_Director,TA,RA,GA,Part_time_faculty,Contractor}
 	int id;
 	Address address;
 	String name;
@@ -82,12 +85,108 @@ public class UniversityPage
 		
 	}
 	
-	public void addCourse(Connection conn, int id)
+	public void addProfessor(Connection conn, int id) throws SQLException
 	{
+		
+		PreparedStatement p = conn.prepareStatement("INSERT INTO public.professor( id, worksfor, designation) VALUES (10, 1, CAST(? AS designation));");
+		
+		p.setString(1, "Associate Director");
+		p.executeUpdate();
+	}
+	
+	public void addCourse(Connection conn, int id) throws SQLException
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter name for course");
+		
+		String name = sc.next();
+		
+		System.out.println("Enter description for course");
+		String description = sc.next();
+		
+		System.out.println("\nSelect a professor to teach this course ");
+		System.out.println("\nHere are a list of professors who are available ");
+				
+		String query="Select id from professor where worksfor=? order by id";
+		PreparedStatement p = conn.prepareStatement(query);
+		p.setInt(1, id);
+		
+		ArrayList<Integer> professors = new ArrayList<>();
+		
+		ResultSet rs = p.executeQuery();
+		if(!rs.isBeforeFirst())
+		{
+			System.out.println("No Professors available to take up new course");
+			return;
+		}
+		else
+		{
+			while(rs.next())
+			{
+				professors.add(rs.getInt(1));
+		
+			}
+		}	
+		
+		for(int i=0;i<professors.size(); i++)
+		{
+			PreparedStatement p2 = conn.prepareStatement("Select name, designation from person p, professor p1 where p.id=? and p.id = p1.id");
+			p2.setInt(1, professors.get(i));
+			
+			ResultSet r2 = p2.executeQuery();
+			
+			while(r2.next())
+			{
+				System.out.println("\n"+professors.get(i)+"\t"+r2.getString(1)+"\t"+r2.getString(2));
+				//System.out.println();
+			}
+			
+				System.out.println("\n Enter Professor id to assign a professor for this course");
+				int pid = sc.nextInt();
+				
+				if(!professors.contains(pid))
+				{
+					System.out.println("Invalid Professor Selected / No such professor");
+					return;
+				}
+				else
+				{
+					query="INSERT INTO public.professorcourse( teaches, taughtby) VALUES (?, ?);";
+				}
+				
+			
+			
+		}
 		
 	}
 	
+	private static void insertCourse(Connection con, int id, String name, String description) throws SQLException
+	{
+		String query = "Select max(id) from course";
+		PreparedStatement getId = con.prepareStatement(query);
+		
+		ResultSet rs = getId.executeQuery();
+		int cid=0;
+		while(rs.next())
+		{
+			cid=rs.getInt(1)+1;
+		}
+		
+		query="INSERT INTO course( id, name, description, offeredby) VALUES (?, ?, ?, ?)";
+		PreparedStatement insert = con.prepareCall(query);
+		insert.setInt(cid, 1);
+		insert.setString(2, name);
+		insert.setString(3, description);
+		insert.setInt(4, id);
+		
+		insert.executeUpdate();
+		
+	}
 	
+	private static void assignProfessor(Connection con, ArrayList<Integer> professors, int id)
+	{
+		
+	}
 	// ONLY FOR UNIT TESTING OF UNIVERSITY FUNCTIONALITY
 	public static void main(String args[]) throws ClassNotFoundException, SQLException
 	{
@@ -108,6 +207,6 @@ public class UniversityPage
 		p.buildUniversity(c, 1);
 		p.listCourses(c, 1);
 		
-		
+		p.addCourse(c, 1);
 	}
 }
